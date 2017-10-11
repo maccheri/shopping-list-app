1bbac97d6ee97363d8150108beb4203592f5d9b3
Felipe Maccheri <felipemaccheri@gmail.com>
2017-10-11 10:32:20 -0300

    Adding Navigation with Event Binding and ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  selectedTab: string;
+
+  onNavigate(feature: string) {
+    this.selectedTab = feature;
+  }
 }
